bc81a8ae40e82c03ae28bbeb738aa0c67092cc67
webreflection <andrea.giammarchi@gmail.com>
2022-10-19 10:40:58 +0200

    Fix #15 - Updated builtin extend check to avoid harakiri

--- index.js
+++ index.js
@@ -8,17 +8,14 @@
         dispatch(records[i]);
       }
     };
-
     var dispatch = function dispatch(_ref) {
       var target = _ref.target,
-          attributeName = _ref.attributeName,
-          oldValue = _ref.oldValue;
+        attributeName = _ref.attributeName,
+        oldValue = _ref.oldValue;
       target.attributeChangedCallback(attributeName, oldValue, target.getAttribute(attributeName));
     };
-
     return function (target, is) {
       var attributeFilter = target.constructor.observedAttributes;
-
       if (attributeFilter) {
         whenDefined(is).then(function () {
           new MutationObserver(attributeChanged).observe(target, {
@@ -26,7 +23,6 @@
             attributeOldValue: true,
             attributeFilter: attributeFilter
           });
-
           for (var i = 0, length = attributeFilter.length; i < length; i++) {
             if (target.hasAttribute(attributeFilter[i])) dispatch({
               target: target,
@@ -36,7 +32,6 @@
           }
         });
       }
-
       return target;
     };
   });
@@ -49,25 +44,18 @@
     if (n === "Map" || n === "Set") return Array.from(o);
     if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
   }
-
   function _arrayLikeToArray(arr, len) {
     if (len == null || len > arr.length) len = arr.length;
-
     for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
-
     return arr2;
   }
-
   function _createForOfIteratorHelper(o, allowArrayLike) {
     var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];
-
     if (!it) {
       if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {
         if (it) o = it;
         var i = 0;
-
         var F = function () {};
-
         return {
           s: F,
           n: function () {
@@ -85,13 +73,11 @@
           f: F
         };
       }
-
       throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
     }
-
     var normalCompletion = true,
-        didErr = false,
-        err;
+      didErr = false,
+      err;
     return {
       s: function () {
         it = it.call(o);
@@ -117,8 +103,9 @@
 
   /*! (c) Andrea Giammarchi - ISC */
   var TRUE = true,
-      FALSE = false,
-      QSA$1 = 'querySelectorAll';
+    FALSE = false,
+    QSA$1 = 'querySelectorAll';
+
   /**
    * Start observing a generic document or root element.
    * @param {(node:Element, connected:boolean) => void} callback triggered per each dis/connected element
@@ -127,20 +114,16 @@
    * @param {string[]} [query=['*']] the selectors to use within nodes
    * @returns {MutationObserver}
    */
-
   var notify = function notify(callback) {
     var root = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : document;
     var MO = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : MutationObserver;
     var query = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : ['*'];
-
     var loop = function loop(nodes, selectors, added, removed, connected, pass) {
       var _iterator = _createForOfIteratorHelper(nodes),
-          _step;
-
+        _step;
       try {
         for (_iterator.s(); !(_step = _iterator.n()).done;) {
           var node = _step.value;
-
           if (pass || QSA$1 in node) {
             if (connected) {
               if (!added.has(node)) {
@@ -153,7 +136,6 @@
               added["delete"](node);
               callback(node, connected);
             }
-
             if (!pass) loop(node[QSA$1](selectors), selectors, added, removed, connected, TRUE);
           }
         }
@@ -163,21 +145,18 @@
         _iterator.f();
       }
     };
-
     var mo = new MO(function (records) {
       if (query.length) {
         var selectors = query.join(',');
         var added = new Set(),
-            removed = new Set();
-
+          removed = new Set();
         var _iterator2 = _createForOfIteratorHelper(records),
-            _step2;
-
+          _step2;
         try {
           for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
             var _step2$value = _step2.value,
-                addedNodes = _step2$value.addedNodes,
-                removedNodes = _step2$value.removedNodes;
+              addedNodes = _step2$value.addedNodes,
+              removedNodes = _step2$value.removedNodes;
             loop(removedNodes, selectors, added, removed, FALSE, FALSE);
             loop(addedNodes, selectors, added, removed, TRUE, FALSE);
           }
@@ -200,48 +179,39 @@
 
   var QSA = 'querySelectorAll';
   var _self$1 = self,
-      document$2 = _self$1.document,
-      Element$1 = _self$1.Element,
-      MutationObserver$2 = _self$1.MutationObserver,
-      Set$2 = _self$1.Set,
-      WeakMap$1 = _self$1.WeakMap;
-
+    document$2 = _self$1.document,
+    Element$1 = _self$1.Element,
+    MutationObserver$2 = _self$1.MutationObserver,
+    Set$2 = _self$1.Set,
+    WeakMap$1 = _self$1.WeakMap;
   var elements = function elements(element) {
     return QSA in element;
   };
-
   var filter = [].filter;
   var qsaObserver = (function (options) {
     var live = new WeakMap$1();
-
     var drop = function drop(elements) {
       for (var i = 0, length = elements.length; i < length; i++) {
         live["delete"](elements[i]);
       }
     };
-
     var flush = function flush() {
       var records = observer.takeRecords();
-
       for (var i = 0, length = records.length; i < length; i++) {
         parse(filter.call(records[i].removedNodes, elements), false);
         parse(filter.call(records[i].addedNodes, elements), true);
       }
     };
-
     var matches = function matches(element) {
       return element.matches || element.webkitMatchesSelector || element.msMatchesSelector;
     };
-
     var notifier = function notifier(element, connected) {
       var selectors;
-
       if (connected) {
         for (var q, m = matches(element), i = 0, length = query.length; i < length; i++) {
           if (m.call(element, q = query[i])) {
             if (!live.has(element)) live.set(element, new Set$2());
             selectors = live.get(element);
-
             if (!selectors.has(q)) {
               selectors.add(q);
               options.handle(element, connected, q);
@@ -256,15 +226,12 @@
         });
       }
     };
-
     var parse = function parse(elements) {
       var connected = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
-
       for (var i = 0, length = elements.length; i < length; i++) {
         notifier(elements[i], connected);
       }
     };
-
     var query = options.query;
     var root = options.root || document$2;
     var observer = notify(notifier, root, MutationObserver$2, query);
@@ -284,41 +251,37 @@
   });
 
   var _self = self,
-      document$1 = _self.document,
-      Map = _self.Map,
-      MutationObserver$1 = _self.MutationObserver,
-      Object$1 = _self.Object,
-      Set$1 = _self.Set,
-      WeakMap = _self.WeakMap,
-      Element = _self.Element,
-      HTMLElement = _self.HTMLElement,
-      Node = _self.Node,
-      Error = _self.Error,
-      TypeError$1 = _self.TypeError,
-      Reflect = _self.Reflect;
+    document$1 = _self.document,
+    Map = _self.Map,
+    MutationObserver$1 = _self.MutationObserver,
+    Object$1 = _self.Object,
+    Set$1 = _self.Set,
+    WeakMap = _self.WeakMap,
+    Element = _self.Element,
+    HTMLElement = _self.HTMLElement,
+    Node = _self.Node,
+    Error = _self.Error,
+    TypeError$1 = _self.TypeError,
+    Reflect = _self.Reflect;
   var defineProperty = Object$1.defineProperty,
-      keys = Object$1.keys,
-      getOwnPropertyNames = Object$1.getOwnPropertyNames,
-      setPrototypeOf = Object$1.setPrototypeOf;
+    keys = Object$1.keys,
+    getOwnPropertyNames = Object$1.getOwnPropertyNames,
+    setPrototypeOf = Object$1.setPrototypeOf;
   var legacy = !self.customElements;
-
   var expando = function expando(element) {
     var key = keys(element);
     var value = [];
     var length = key.length;
-
     for (var i = 0; i < length; i++) {
       value[i] = element[key[i]];
       delete element[key[i]];
     }
-
     return function () {
       for (var _i = 0; _i < length; _i++) {
         element[key[_i]] = value[_i];
       }
     };
   };
-
   if (legacy) {
     var HTMLBuiltIn = function HTMLBuiltIn() {
       var constructor = this.constructor;
@@ -328,21 +291,17 @@
       var element = createElement.call(document$1, is);
       return augment(setPrototypeOf(element, constructor.prototype), is);
     };
-
     var createElement = document$1.createElement;
     var classes = new Map();
     var defined = new Map();
     var prototypes = new Map();
     var registry = new Map();
     var query = [];
-
     var handle = function handle(element, connected, selector) {
       var proto = prototypes.get(selector);
-
       if (connected && !proto.isPrototypeOf(element)) {
         var redefine = expando(element);
         override = setPrototypeOf(element, proto);
-
         try {
           new proto.constructor();
         } finally {
@@ -350,35 +309,28 @@
           redefine();
         }
       }
-
       var method = "".concat(connected ? '' : 'dis', "connectedCallback");
       if (method in proto) element[method]();
     };
-
     var _qsaObserver = qsaObserver({
-      query: query,
-      handle: handle
-    }),
-        parse = _qsaObserver.parse;
-
+        query: query,
+        handle: handle
+      }),
+      parse = _qsaObserver.parse;
     var override = null;
-
     var whenDefined = function whenDefined(name) {
       if (!defined.has(name)) {
         var _,
-            $ = new Promise(function ($) {
-          _ = $;
-        });
-
+          $ = new Promise(function ($) {
+            _ = $;
+          });
         defined.set(name, {
           $: $,
           _: _
         });
       }
-
       return defined.get(name).$;
     };
-
     var augment = attributesObserver(whenDefined, MutationObserver$1);
     defineProperty(self, 'customElements', {
       configurable: true,
@@ -392,7 +344,6 @@
           whenDefined(is).then(function () {
             parse(document$1.querySelectorAll(is));
           });
-
           defined.get(is)._(Class);
         },
         get: function get(is) {
@@ -415,9 +366,9 @@
         var Class = is ? registry.get(is) : registry.get(name);
         return Class ? new Class() : createElement.call(document$1, name);
       }
-    }); // in case ShadowDOM is used through a polyfill, to avoid issues
+    });
+    // in case ShadowDOM is used through a polyfill, to avoid issues
     // with builtin extends within shadow roots
-
     if (!('isConnected' in Node.prototype)) defineProperty(Node.prototype, 'isConnected', {
       configurable: true,
       get: function get() {
@@ -425,82 +376,67 @@
       }
     });
   } else {
-    try {
-      var LI = function LI() {
-        return self.Reflect.construct(HTMLLIElement, [], LI);
-      };
-
-      LI.prototype = HTMLLIElement.prototype;
-      var is = 'extends-li';
-      self.customElements.define('extends-li', LI, {
-        'extends': 'li'
-      });
-      legacy = document$1.createElement('li', {
-        is: is
-      }).outerHTML.indexOf(is) < 0;
-      var _self$customElements = self.customElements,
+    legacy = !self.customElements.get('extends-li');
+    if (legacy) {
+      try {
+        var LI = function LI() {
+          return self.Reflect.construct(HTMLLIElement, [], LI);
+        };
+        LI.prototype = HTMLLIElement.prototype;
+        var is = 'extends-li';
+        self.customElements.define('extends-li', LI, {
+          'extends': 'li'
+        });
+        legacy = document$1.createElement('li', {
+          is: is
+        }).outerHTML.indexOf(is) < 0;
+        var _self$customElements = self.customElements,
           get = _self$customElements.get,
           _whenDefined = _self$customElements.whenDefined;
-      defineProperty(self.customElements, 'whenDefined', {
-        configurable: true,
-        value: function value(is) {
-          var _this = this;
-
-          return _whenDefined.call(this, is).then(function (Class) {
-            return Class || get.call(_this, is);
-          });
-        }
-      });
-    } catch (o_O) {
-      legacy = !legacy;
+        defineProperty(self.customElements, 'whenDefined', {
+          configurable: true,
+          value: function value(is) {
+            var _this = this;
+            return _whenDefined.call(this, is).then(function (Class) {
+              return Class || get.call(_this, is);
+            });
+          }
+        });
+      } catch (o_O) {}
     }
   }
-
   if (legacy) {
     var parseShadow = function parseShadow(element) {
       var root = shadowRoots.get(element);
-
       _parse(root.querySelectorAll(this), element.isConnected);
     };
-
     var customElements = self.customElements;
     var _createElement = document$1.createElement;
     var define = customElements.define,
-        _get = customElements.get,
-        upgrade = customElements.upgrade;
-
+      _get = customElements.get,
+      upgrade = customElements.upgrade;
     var _ref = Reflect || {
-      construct: function construct(HTMLElement) {
-        return HTMLElement.call(this);
-      }
-    },
-        construct = _ref.construct;
-
+        construct: function construct(HTMLElement) {
+          return HTMLElement.call(this);
+        }
+      },
+      construct = _ref.construct;
     var shadowRoots = new WeakMap();
     var shadows = new Set$1();
-
     var _classes = new Map();
-
     var _defined = new Map();
-
     var _prototypes = new Map();
-
     var _registry = new Map();
-
     var shadowed = [];
     var _query = [];
-
     var getCE = function getCE(is) {
       return _registry.get(is) || _get.call(customElements, is);
     };
-
     var _handle = function _handle(element, connected, selector) {
       var proto = _prototypes.get(selector);
-
       if (connected && !proto.isPrototypeOf(element)) {
         var redefine = expando(element);
         _override = setPrototypeOf(element, proto);
-
         try {
           new proto.constructor();
         } finally {
@@ -508,79 +444,64 @@
           redefine();
         }
       }
-
       var method = "".concat(connected ? '' : 'dis', "connectedCallback");
       if (method in proto) element[method]();
     };
-
     var _qsaObserver2 = qsaObserver({
-      query: _query,
-      handle: _handle
-    }),
-        _parse = _qsaObserver2.parse;
-
+        query: _query,
+        handle: _handle
+      }),
+      _parse = _qsaObserver2.parse;
     var _qsaObserver3 = qsaObserver({
-      query: shadowed,
-      handle: function handle(element, connected) {
-        if (shadowRoots.has(element)) {
-          if (connected) shadows.add(element);else shadows["delete"](element);
-          if (_query.length) parseShadow.call(_query, element);
+        query: shadowed,
+        handle: function handle(element, connected) {
+          if (shadowRoots.has(element)) {
+            if (connected) shadows.add(element);else shadows["delete"](element);
+            if (_query.length) parseShadow.call(_query, element);
+          }
         }
-      }
-    }),
-        parseShadowed = _qsaObserver3.parse; // qsaObserver also patches attachShadow
+      }),
+      parseShadowed = _qsaObserver3.parse;
+    // qsaObserver also patches attachShadow
     // be sure this runs *after* that
-
-
     var attachShadow = Element.prototype.attachShadow;
     if (attachShadow) Element.prototype.attachShadow = function (init) {
       var root = attachShadow.call(this, init);
       shadowRoots.set(this, root);
       return root;
     };
-
     var _whenDefined2 = function _whenDefined2(name) {
       if (!_defined.has(name)) {
         var _,
-            $ = new Promise(function ($) {
-          _ = $;
-        });
-
+          $ = new Promise(function ($) {
+            _ = $;
+          });
         _defined.set(name, {
           $: $,
           _: _
         });
       }
-
       return _defined.get(name).$;
     };
-
     var _augment = attributesObserver(_whenDefined2, MutationObserver$1);
-
     var _override = null;
     getOwnPropertyNames(self).filter(function (k) {
       return /^HTML.*Element$/.test(k);
     }).forEach(function (k) {
       var HTMLElement = self[k];
-
       function HTMLBuiltIn() {
         var constructor = this.constructor;
         if (!_classes.has(constructor)) throw new TypeError$1('Illegal constructor');
-
         var _classes$get = _classes.get(constructor),
-            is = _classes$get.is,
-            tag = _classes$get.tag;
-
+          is = _classes$get.is,
+          tag = _classes$get.tag;
         if (is) {
           if (_override) return _augment(_override, is);
-
           var element = _createElement.call(document$1, tag);
-
           element.setAttribute('is', is);
           return _augment(setPrototypeOf(element, constructor.prototype), is);
         } else return construct.call(this, HTMLElement, [], constructor);
       }
-
 
       defineProperty(HTMLBuiltIn.prototype = HTMLElement.prototype, 'constructor', {
         value: HTMLBuiltIn
@@ -593,15 +514,11 @@
       configurable: true,
       value: function value(name, options) {
         var is = options && options.is;
-
         if (is) {
           var Class = _registry.get(is);
-
           if (Class && _classes.get(Class).tag === name) return new Class();
         }
-
         var element = _createElement.call(document$1, name);
-
         if (is) element.setAttribute('is', is);
         return element;
       }
@@ -618,20 +535,16 @@
       configurable: true,
       value: function value(element) {
         var is = element.getAttribute('is');
-
         if (is) {
           var _constructor = _registry.get(is);
-
           if (_constructor) {
-            _augment(setPrototypeOf(element, _constructor.prototype), is); // apparently unnecessary because this is handled by qsa observer
+            _augment(setPrototypeOf(element, _constructor.prototype), is);
+            // apparently unnecessary because this is handled by qsa observer
             // if (element.isConnected && element.connectedCallback)
             //   element.connectedCallback();
-
-
             return;
           }
         }
-
         upgrade.call(customElements, element);
       }
     });
@@ -641,7 +554,6 @@
         if (getCE(is)) throw new Error("'".concat(is, "' has already been defined as a custom element"));
         var selector;
         var tag = options && options["extends"];
-
         _classes.set(Class, tag ? {
           is: is,
           tag: tag
@@ -649,28 +561,21 @@
           is: '',
           tag: is
         });
-
         if (tag) {
           selector = "".concat(tag, "[is=\"").concat(is, "\"]");
-
           _prototypes.set(selector, Class.prototype);
-
           _registry.set(is, Class);
-
           _query.push(selector);
         } else {
           define.apply(customElements, arguments);
           shadowed.push(selector = is);
         }
-
         _whenDefined2(is).then(function () {
           if (tag) {
             _parse(document$1.querySelectorAll(selector));
-
             shadows.forEach(parseShadow, [selector]);
           } else parseShadowed(document$1.querySelectorAll(selector));
         });
-
         _defined.get(is)._(Class);
       }
     });
